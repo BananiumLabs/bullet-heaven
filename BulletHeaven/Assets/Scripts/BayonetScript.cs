3b779566dbe542fc1b1bc41ec0c2623e7ea20eeb
Omar Hossain <omarepic1@gmail.com>
2019-07-11 17:37:18 -0700

    Bayonet

---
+++ BulletHeaven/Assets/Scripts/BayonetScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BayonetScript : MonoBehaviour
+{
+    public float speedRotate;
+    public float time;
+
+    public AudioSource slash;
+
+    //public Quaternion initalRotation;
+    //private bool inital;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Die());
+        //initalRotation = transform.rotation;
+        //inital = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = GameObject.Find("Player").GetComponent<Transform>().position;
+        transform.Rotate(Vector3.forward * speedRotate * Time.deltaTime);
+        
+        //Debug.Log(transform.rotation.z);
+        //if(transform.rotation.z <= 0f)
+           // Destroy(gameObject);
+    }
+
+    IEnumerator Die()
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D col) 
+    {
+        slash.Play(0);
+    }
+}
--- BulletHeaven/Assets/Scripts/BayonetSpawner.cs
+++ BulletHeaven/Assets/Scripts/BayonetSpawner.cs
@@ -2,32 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HaloSpawner : MonoBehaviour
+public class BayonetSpawner : MonoBehaviour
 {
-    public GameObject Halo;
-    private AudioSource HaloSource;
-    public float nextHalo, timeBtwnHalos;
-    private Vector3 spawnPos;
-    public float spawnDistance;
-
+    public GameObject Bayonet;
+    private AudioSource BayonetSource;
+    public float nextBayonet, timeBtwnBayonets;
+    
     // Start is called before the first frame update
     void Start()
     {
-        HaloSource = GetComponent<AudioSource>();
+        BayonetSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        spawnPos = transform.position + (transform.up*spawnDistance);
+
         if(GameObject.Find("Player").GetComponent<PlayerHealth>().halos > 0) {
             if(Input.GetKey(KeyCode.L) || Input.GetKey("joystick button 3")|| Input.GetKey("joystick button 0"))
             {
-                if (Time.time > nextHalo)
+                if (Time.time > nextBayonet)
                 {
-                    nextHalo = Time.time + timeBtwnHalos;
-                    GameObject halo = Instantiate(Halo, spawnPos, transform.rotation) as GameObject;
-                    HaloSource.Play();
+                    nextBayonet = Time.time + timeBtwnBayonets;
+                    GameObject halo = Instantiate(Bayonet, transform.position, transform.rotation) as GameObject;
+                    BayonetSource.Play();
                     GameObject.Find("Player").GetComponent<PlayerHealth>().halos--;
                 }
 
