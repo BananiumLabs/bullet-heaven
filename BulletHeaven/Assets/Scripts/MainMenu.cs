b9489e083a6860d68452e5e8bb7826bbd2669138
Ben Cuan <github@bencuan.me>
2019-07-25 19:01:11 -0700

    Empty help page

--- BulletHeaven/Assets/Scripts/MainMenu.cs
+++ BulletHeaven/Assets/Scripts/MainMenu.cs
@@ -1,51 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
-{
+public class MainMenu : MonoBehaviour {
 
     public Image mapOverlay;
 
     // public SpriteRenderer hudOverlay;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start () {
+
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update () {
+
     }
 
-    public void StartGame() {
-        ScoreCounter.ResetValues();
-        StartCoroutine(FadeImages());
+    public void StartGame () {
+        ScoreCounter.ResetValues ();
+        StartCoroutine (FadeImages ());
     }
 
     /// Fade the hud overlay out while simultaneously fading in the map overlay
-    IEnumerator FadeImages() {
+    IEnumerator FadeImages () {
 
-        while(mapOverlay.color.a < 1f) {
-            mapOverlay.color = new Color(mapOverlay.color.r, mapOverlay.color.g, mapOverlay.color.b, mapOverlay.color.a + 0.01f);
+        while (mapOverlay.color.a < 1f) {
+            mapOverlay.color = new Color (mapOverlay.color.r, mapOverlay.color.g, mapOverlay.color.b, mapOverlay.color.a + 0.01f);
             // hudOverlay.color = new Color(hudOverlay.color.r, hudOverlay.color.g, hudOverlay.color.b, hudOverlay.color.a + 0.01f);
-            yield return new WaitForSeconds(0.02f);
+            yield return new WaitForSeconds (0.02f);
         }
 
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene (1);
     }
 
-
-    public void Quit() {
-        Application.Quit();
+    public void Quit () {
+        Application.Quit ();
     }
 
-    public void ReturnToMenu() {
-        SceneManager.LoadScene(0);
+    public void ReturnToMenu () {
+        SceneManager.LoadScene (0);
+    }
+
+    public void ToggleHelp () {
+        GameObject.Find ("Help Canvas").GetComponent<Canvas> ().enabled = !GameObject.Find ("Help Canvas").GetComponent<Canvas> ().enabled;
     }
 }
